f13ab0b0d4771f15c084bdcf232bba5090f801ba
FORNO <forno@xmaho.link>
2020-03-13 19:45:27 +0900

    Add csv reading from stdin

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,14 @@
 #include <filesystem>
 #include <iostream>
 
+#include <boost/optional.hpp>
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/ini_parser.hpp>
-#include <boost/optional.hpp>
+#include <boost/tokenizer.hpp>
 
 #include <Eigen/Dense>
+
+constexpr std::size_t marker_count {29};
 
 struct index_holder
 {
@@ -17,7 +20,7 @@ struct index_holder
 
 index_holder read_indexies(std::filesystem::path path);
 
-int main([[maybe_unused]] int argc, [[maybe_unused]] char** argv)
+int main(int argc, char** argv)
 {
   if (argc < 2) {
     std::cerr << "Usage: " << *argv << " IndexiesFilePath\n";
@@ -26,6 +29,16 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char** argv)
 
   try {
     const auto indexies {read_indexies(argv[1])};
+    for (std::string line; std::getline(std::cin, line);) {
+      boost::tokenizer<boost::escaped_list_separator<char>> tokens(line);
+      Eigen::Matrix<float, marker_count, 3> values;
+      std::size_t current_index {0};
+      for (auto v : tokens) {
+        values(current_index / 3, current_index % 3) = std::stof(v);
+        ++current_index;
+      }
+      std::cout << values;
+    }
   } catch(boost::wrapexcept<boost::property_tree::ptree_bad_path>& e) {
     std::cerr << "Invalid indexies format: " << e.what() << '\n';
     return 1;
