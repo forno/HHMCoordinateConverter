dba7e1417fd6f3856ce33bb492e5387f65f40394
FORNO <forno@xmaho.link>
2020-03-13 19:12:08 +0900

    Add indexies load

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+#include <cstddef>
+#include <filesystem>
 #include <iostream>
 
 #include <boost/property_tree/ptree.hpp>
@@ -6,19 +8,39 @@
 
 #include <Eigen/Dense>
 
+struct index_holder
+{
+  std::size_t lasis;
+  std::size_t rasis;
+  std::size_t vsacral;
+};
+
+index_holder read_indexies(std::filesystem::path path);
+
 int main([[maybe_unused]] int argc, [[maybe_unused]] char** argv)
 {
   if (argc < 2) {
     std::cerr << "Usage: " << *argv << " IndexiesFilePath\n";
     return 1;
   }
-  using namespace boost::property_tree;
 
-  ptree pt;
-  read_ini(argv[1], pt);
-
-  if (auto value = pt.get_optional<int>("Indexies.FrontHead")) {
-    std::cout << *value << std::endl;
+  try {
+    const auto indexies {read_indexies(argv[1])};
+  } catch(boost::wrapexcept<boost::property_tree::ptree_bad_path>& e) {
+    std::cerr << "Invalid indexies format: " << e.what() << '\n';
+    return 1;
+  } catch(boost::wrapexcept<boost::property_tree::ini_parser::ini_parser_error>& e) {
+    std::cerr << "Invalid indexies file: " << e.what() << '\n';
+    return 1;
   }
+
   return 0;
+}
+
+index_holder read_indexies(std::filesystem::path path)
+{
+  using namespace boost::property_tree;
+  ptree pt;
+  read_ini(path.native(), pt);
+  return index_holder {pt.get<std::size_t>("Indexies.lasis"), pt.get<std::size_t>("Indexies.rasis"), pt.get<std::size_t>("Indexies.vsacral")};
 }
