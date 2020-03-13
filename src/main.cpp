09bcf36f76444e9bfd0f156200a465617f58219e
FORNO <forno@xmaho.link>
2020-03-13 18:42:16 +0900

    Add ini file reader

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,24 @@
+#include <iostream>
+
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/ini_parser.hpp>
+#include <boost/optional.hpp>
+
 #include <Eigen/Dense>
 
 int main([[maybe_unused]] int argc, [[maybe_unused]] char** argv)
 {
+  if (argc < 2) {
+    std::cerr << "Usage: " << *argv << " IndexiesFilePath\n";
+    return 1;
+  }
+  using namespace boost::property_tree;
+
+  ptree pt;
+  read_ini(argv[1], pt);
+
+  if (auto value = pt.get_optional<int>("Indexies.FrontHead")) {
+    std::cout << *value << std::endl;
+  }
   return 0;
 }
