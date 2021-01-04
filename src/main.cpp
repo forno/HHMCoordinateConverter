8604eca95ccdc11ce730344d54685cbb267995a2
FORNO <forno@xmaho.link>
2021-01-04 16:52:19 +0900

    Reformat code
    Signed-off-by: FORNO <forno@xmaho.link>

--- src/main.cpp
+++ src/main.cpp
@@ -36,7 +36,7 @@ SOFTWARE.
 #include <Eigen/Dense>
 #include <Eigen/Geometry>
 
-constexpr std::size_t marker_count {29};
+constexpr std::size_t marker_count{29};
 
 struct index_holder
 {
@@ -47,7 +47,7 @@ struct index_holder
 
 index_holder read_indexies(std::filesystem::path path);
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
   std::ios::sync_with_stdio(false);
   std::cin.tie(nullptr);
@@ -56,39 +56,42 @@ int main(int argc, char** argv)
   namespace po = boost::program_options;
 
   po::options_description description("Options");
-  description.add_options()
-    ("header,h", po::value<bool>()->default_value(true), "Dose the CSV has header?")
-    ("config,c", po::value<std::string>(), "Config file path")
-    ("l2w,l", po::value<bool>()->default_value(false), "Local to World flag")
-    ("zup,z", po::value<bool>()->default_value(false), "Z up coordinate")
-    ("help,H", "Help")
-    ;
+  description.add_options()("header,h", po::value<bool>()->default_value(true), "Dose the CSV has header?")("config,c", po::value<std::string>(), "Config file path")("l2w,l", po::value<bool>()->default_value(false), "Local to World flag")("zup,z", po::value<bool>()->default_value(false), "Z up coordinate")("help,H", "Help");
 
   po::variables_map vm;
   po::store(po::parse_command_line(argc, argv, description), vm);
   po::notify(vm);
 
-  if (argc < 2 || vm.count("help")) {
+  if (argc < 2 || vm.count("help"))
+  {
     std::cerr << "Usage: " << *argv << " --config config/Cooking < cooking.csv\n";
     return 1;
   }
 
-  try {
+  try
+  {
     const auto config_path = vm["config"].as<std::string>();
-    const auto indexies {read_indexies(config_path)};
-    if (vm["header"].as<bool>()) {
+    const auto indexies{read_indexies(config_path)};
+    if (vm["header"].as<bool>())
+    {
       std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     }
-    if (!vm["l2w"].as<bool>()) {
-      for (std::string line; std::getline(std::cin, line);) {
+    if (!vm["l2w"].as<bool>())
+    {
+      for (std::string line; std::getline(std::cin, line);)
+      {
         boost::tokenizer<boost::escaped_list_separator<char>> tokens(line);
         Eigen::Matrix<float, marker_count, 3, Eigen::RowMajor> values;
         {
           auto it = tokens.begin();
-          for (std::size_t i {0}; i < marker_count * 3; ++i) {
-            try {
+          for (std::size_t i{0}; i < marker_count * 3; ++i)
+          {
+            try
+            {
               values(i) = std::stof(*it);
-            } catch (std::invalid_argument&) {
+            }
+            catch (std::invalid_argument &)
+            {
               values(i) = std::numeric_limits<float>::quiet_NaN();
             }
             ++it;
@@ -96,7 +99,8 @@ int main(int argc, char** argv)
         }
 
         const Eigen::Vector3f center_asis = values.row(indexies.left_asis) / 2 + values.row(indexies.right_asis) / 2;
-        for (std::size_t i {0}; i < marker_count; ++i) {
+        for (std::size_t i{0}; i < marker_count; ++i)
+        {
           values.row(i) -= center_asis;
         }
 
@@ -106,84 +110,113 @@ int main(int argc, char** argv)
         const auto wrong_up_direction = w2l_rotation * coordinate_up_direction;
         const auto up_fix_rotation = Eigen::Quaternionf::FromTwoVectors(wrong_up_direction, coordinate_up_direction);
 
-        for (std::size_t i {0}; i < marker_count; ++i) {
+        for (std::size_t i{0}; i < marker_count; ++i)
+        {
           values.row(i) = up_fix_rotation * w2l_rotation * values.row(i);
         }
 
-        auto is_first {true};
-        for (std::size_t i {0}; i < static_cast<std::size_t>(values.size()); ++i) {
-          if (!std::exchange(is_first, false)) {
+        auto is_first{true};
+        for (std::size_t i{0}; i < static_cast<std::size_t>(values.size()); ++i)
+        {
+          if (!std::exchange(is_first, false))
+          {
             std::cout.put(',');
           }
-          if (!isnan(values(i))) {
+          if (!isnan(values(i)))
+          {
             std::cout << values(i);
           }
         }
-        const auto l2w_translation {-center_asis};
-        for (std::size_t i {0}; i < static_cast<std::size_t>(l2w_translation.size()); ++i) {
+        const auto l2w_translation{-center_asis};
+        for (std::size_t i{0}; i < static_cast<std::size_t>(l2w_translation.size()); ++i)
+        {
           std::cout << ',' << l2w_translation(i);
         }
-        const auto l2w_rotation {w2l_rotation.inverse()};
-        const auto& l2w_coeffs {l2w_rotation.coeffs()};
-        for (std::size_t i {0}; i < static_cast<std::size_t>(l2w_coeffs.size()); ++i) {
+        const auto l2w_rotation{w2l_rotation.inverse()};
+        const auto &l2w_coeffs{l2w_rotation.coeffs()};
+        for (std::size_t i{0}; i < static_cast<std::size_t>(l2w_coeffs.size()); ++i)
+        {
           std::cout << ',' << l2w_coeffs(i);
         }
         std::cout.put('\n');
       }
-    } else {
-      for (std::string line; std::getline(std::cin, line);) {
+    }
+    else
+    {
+      for (std::string line; std::getline(std::cin, line);)
+      {
         Eigen::Matrix<float, marker_count, 3, Eigen::RowMajor> values;
         Eigen::Vector3f translation;
         Eigen::Quaternionf rotation;
         {
           boost::tokenizer<boost::escaped_list_separator<char>> tokens(line);
           auto it = tokens.begin();
-          for (std::size_t i {0}; i < static_cast<std::size_t>(values.size()); ++i) {
-            try {
+          for (std::size_t i{0}; i < static_cast<std::size_t>(values.size()); ++i)
+          {
+            try
+            {
               values(i) = std::stof(*it);
-            } catch (std::invalid_argument&) {
+            }
+            catch (std::invalid_argument &)
+            {
               values(i) = std::numeric_limits<float>::quiet_NaN();
             }
             ++it;
           }
-          for (std::size_t i {0}; i < static_cast<std::size_t>(translation.size()); ++i) {
-            try {
+          for (std::size_t i{0}; i < static_cast<std::size_t>(translation.size()); ++i)
+          {
+            try
+            {
               translation(i) = std::stof(*it);
-            } catch (std::invalid_argument&) {
+            }
+            catch (std::invalid_argument &)
+            {
               translation(i) = std::numeric_limits<float>::quiet_NaN();
             }
             ++it;
           }
-          Eigen::Vector4f rotation_coeffs ;
-          for (std::size_t i {0}; i < static_cast<std::size_t>(rotation_coeffs.size()); ++i) {
-            try {
+          Eigen::Vector4f rotation_coeffs;
+          for (std::size_t i{0}; i < static_cast<std::size_t>(rotation_coeffs.size()); ++i)
+          {
+            try
+            {
               rotation_coeffs(i) = std::stof(*it);
-            } catch (std::invalid_argument&) {
+            }
+            catch (std::invalid_argument &)
+            {
               rotation_coeffs(i) = std::numeric_limits<float>::quiet_NaN();
             }
             ++it;
           }
           rotation = Eigen::Quaternionf(rotation_coeffs);
         }
-        for (std::size_t i {0}; i < marker_count; ++i) {
+        for (std::size_t i{0}; i < marker_count; ++i)
+        {
           values.row(i) = rotation * values.row(i) + translation;
         }
-        auto is_first {true};
-        for (std::size_t i {0}; i < static_cast<std::size_t>(values.size()); ++i) {
-          if (!std::exchange(is_first, false)) {
+        auto is_first{true};
+        for (std::size_t i{0}; i < static_cast<std::size_t>(values.size()); ++i)
+        {
+          if (!std::exchange(is_first, false))
+          {
             std::cout.put(',');
           }
-          if (!isnan(values(i))) {
+          if (!isnan(values(i)))
+          {
             std::cout << values(i);
           }
         }
         std::cout.put('\n');
       }
     }
-  } catch (boost::wrapexcept<boost::property_tree::ptree_bad_path>& e) {
+  }
+  catch (boost::wrapexcept<boost::property_tree::ptree_bad_path> &e)
+  {
     std::cerr << "Invalid indexies format: " << e.what() << '\n';
     return 1;
-  } catch (boost::wrapexcept<boost::property_tree::ini_parser::ini_parser_error>& e) {
+  }
+  catch (boost::wrapexcept<boost::property_tree::ini_parser::ini_parser_error> &e)
+  {
     std::cerr << "Invalid indexies file: " << e.what() << '\n';
     return 1;
   }
@@ -196,5 +229,5 @@ index_holder read_indexies(std::filesystem::path path)
   using namespace boost::property_tree;
   ptree pt;
   read_ini(path.native(), pt);
-  return index_holder {pt.get<std::size_t>("Indexies.LeftAsis"), pt.get<std::size_t>("Indexies.RightAsis"), pt.get<std::size_t>("Indexies.VSacral")};
+  return index_holder{pt.get<std::size_t>("Indexies.LeftAsis"), pt.get<std::size_t>("Indexies.RightAsis"), pt.get<std::size_t>("Indexies.VSacral")};
 }
